34082a670e2e6365415d68f90f224c57301098ab
Apple <apple@Apples-MacBook-Pro.local>
2023-09-21 00:28:50 +0500

    first commit

---
+++ app/src/main/java/com/kids/easymathapp/model/StoreSetModel.java
@@ -0,0 +1,13 @@
+package com.kids.easymathapp.model;
+
+public class StoreSetModel {
+
+    public String title, category;
+    public int id;
+
+    public StoreSetModel(int id, String title, String category) {
+        this.id = id;
+        this.title = title;
+        this.category = category;
+    }
+}
